98f8c4720a0a60b54b6dc090583c29d7f4143c5a
Rahmat Sulistio <rr7465457@gmail.com>
2023-11-08 10:41:41 +0700

    :bento: Add fonts and photo profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: 'Rowdies, sans-serif',
+        body: 'Rubik, sans-serif',
+      },
+    },
   },
   plugins: [],
 }
