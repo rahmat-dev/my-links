9562bc2a24d3e08b818892bcc95da6eb49424081
Rahmat Sulistio <rr7465457@gmail.com>
2023-11-09 02:29:38 +0700

    :lipstick: Add react typing animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TypeAnimation } from 'react-type-animation'
 import { ButtonLink } from './components/button-link'
 import { mainLinks, socialLinks } from './data'
 
@@ -5,14 +6,29 @@ export default function App() {
   return (
     <main className="grid min-h-screen w-full place-items-center bg-teal-300 p-4">
       <div className="w-full max-w-md rounded-xl border-2 border-black bg-neutral-100 p-6 shadow-[5px_5px_black]">
-        <div className="-mt-20">
+        <div className="-mt-20 text-center">
           <img
             src="/me.webp"
             className="mx-auto aspect-square w-40 rounded-lg border-2 border-black object-cover shadow-[5px_5px_black]"
           />
-          <h1 className="mt-4 text-center font-display text-2xl uppercase">
+          <h1 className="mt-4 font-display text-2xl uppercase">
             Rahmat Sulistio
           </h1>
+          <TypeAnimation
+            sequence={[
+              'Freelance Developer',
+              2000,
+              'Full-Stack Developer',
+              2000,
+              'Web Developer',
+              2000,
+              'Mobile App Developer',
+              2000,
+            ]}
+            speed={50}
+            repeat={Infinity}
+            className="font-body text-lg"
+          />
         </div>
 
         <div className="mt-10 flex flex-col items-center gap-5">
@@ -29,7 +45,7 @@ export default function App() {
           ))}
         </div>
 
-        <div className="mt-12 flex justify-center gap-4">
+        <div className="mt-16 flex justify-center gap-4">
           {socialLinks.map(({ href, icon, bg }) => (
             <ButtonLink
               key={href}
